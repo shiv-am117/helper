476c0a2dc3699d12f3d3cbe3889e1904c5f53111
shiv-am117 <goel.shivam117@gmail.com>
2018-11-25 17:36:33 +0530

    react-added

---
+++ helper.js
@@ -0,0 +1,10 @@
+class Greeting extends React.Component {
+    render() {
+        return (<p>Hello world</p>);
+    }
+}
+ReactDOM.render(
+    <Greeting />,
+    document.getElementById('contact')
+);
+</script>
